ae3ed88b5508be32cac9430d2c513afc56a52ed7
John Mastro <john.b.mastro@gmail.com>
2018-06-22 12:19:33 -0700

    Update dependencies and build machinery

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
-var path = require('path');
-var SRC_DIR = path.join(__dirname, '/client');
-var DIST_DIR = path.join(__dirname, '/client/dist');
+const path = require("path");
+const SRC_DIR = path.join(__dirname, "/client/src");
+const DIST_DIR = path.join(__dirname, "/client/dist");
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: {
+    app: `${SRC_DIR}/index.jsx`
+  },
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: DIST_DIR
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
+        use: [
+          {
+            loader: "babel-loader",
+            // query: {
+            //   presets: ["react", "env"]
+            // }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
 };
+
